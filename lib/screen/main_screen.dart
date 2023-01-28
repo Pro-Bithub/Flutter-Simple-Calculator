fd3cc7c9ad6a57511ec04ec9501d6ed8f92d7aa7
Hatem ghozzi <35577969+Pro-Bithub@users.noreply.github.com>
2023-01-28 10:57:17 +0100

    Update main_screen.dart

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -34,10 +34,6 @@ class MainScreen extends StatelessWidget {
   ];
 
   /////////////////////////////////////
-  //@CodeWithFlexz on Instagram
-  //
-  //AmirBayat0 on Github
-  //Programming with Flexz on Youtube
   /////////////////////////////////////
   @override
   Widget build(BuildContext context) {
